5966b6f0af87a96bde728247657bdb7cca591c98
Gateston Johns <gateston136@gmail.com>
2023-10-29 14:58:32 -0400

    get parcel info on parcel click

--- lib/components/map_widget.dart
+++ lib/components/map_widget.dart
@@ -6,11 +6,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
-
-
-
-
 class MyMapWidget extends StatefulWidget {
   @override
   _MapWidgetState createState() => _MapWidgetState();
@@ -31,7 +26,7 @@ class _MapWidgetState extends State<MyMapWidget> {
         ),
         onCameraIdle: mapUpdate,
         onMapCreated: onMap,
-        polygons: _polygon,
+        polygons: _polygon
     );
 
     return map;
@@ -45,7 +40,7 @@ class _MapWidgetState extends State<MyMapWidget> {
       "lat2": bounds.northeast.latitude.toString(),
       "long2": bounds.northeast.longitude.toString()
     };
-    final uri = Uri.http("3.88.148.163", '', queryParams);
+    final uri = Uri.http("3.94.113.50", '', queryParams);
     final response = await http.get(uri);
 
     Map<String, dynamic> data = json.decode(response.body);
@@ -54,6 +49,7 @@ class _MapWidgetState extends State<MyMapWidget> {
     _polygon.clear();
     for (dynamic i in data['parcels']){
       List<LatLng> latlngs = [];
+      var objID = i['objectid'];
       var polyText = i['st_astext'].toString();
       polyText = polyText.replaceAll(RegExp("([a-zA-Z()])*"), "");
 
@@ -62,12 +58,13 @@ class _MapWidgetState extends State<MyMapWidget> {
         latlngs.add(LatLng(double.parse(k[1]), double.parse(k[0])));
       }
 
-      map.polygons.add(Polygon(polygonId: PolygonId(id.toString())
+      map.polygons.add(Polygon(polygonId: PolygonId(objID.toString())
         , points: latlngs
         , fillColor: Colors.green.withOpacity(0.3)
         , strokeColor: Colors.green
         , geodesic: true
-        , strokeWidth: 4,));
+        , strokeWidth: 4
+        , onTap: () => _onParcelTap(objID)));
       id++;
     }
     
@@ -79,4 +76,13 @@ class _MapWidgetState extends State<MyMapWidget> {
     _control = controller;
   }
 
+  void _onParcelTap(int polygonId) async {
+    print("Parcel with ID: $polygonId was clicked!");
+
+    final parceluri = Uri.http('3.94.113.50','/parcel', {"objectid": polygonId.toString()});
+    final response = await http.get(parceluri);
+
+    var data = json.decode(response.body);
+    print("returned info: $data");
+  }
 }
